303f42e95bbc4f2766ed052c8c253a56f8a524f5
jobairahmed <ryleejane663@gmail.com>
2020-11-28 14:57:08 +0600

    login add

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <title>Admincast bootstrap 4 &amp; angular 5 admin template, Шаблон админки | Login</title>
+    <!-- GLOBAL MAINLY STYLES-->
+    <link href="{{asset('asset')}}/assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="./assets/vendors/themify-icons/css/themify-icons.css" rel="stylesheet" />
+    <!-- THEME STYLES-->
+<link href="{{asset('asset')}}/assets/css/main.css" rel="stylesheet" />
+    <!-- PAGE LEVEL STYLES-->
+<link href="{{asset('asset')}}/assets/css/pages/auth-light.css" rel="stylesheet" />
+</head>
+
+<body class="bg-dark">
+    <div class="content">
+        <div class="pt-5 mt-5">
+            <h2 class="text-center text-white mb-5" >Welcome To POS System </h2>
+        </div>
+    <form id="login-form" action="{{route('login')}}" method="post">
+        @csrf
+            <h2 class="login-title">Log in</h2>
+            <div class="form-group">
+                <div class="input-group-icon right">
+                    <div class="input-icon"><i class="fa fa-envelope"></i></div>
+                    <input class="form-control" type="email" name="email" placeholder="Email" autocomplete="off">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="input-group-icon right">
+                    <div class="input-icon"><i class="fa fa-lock font-16"></i></div>
+                    <input class="form-control" type="password" name="password" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group d-flex justify-content-between">
+                <label class="ui-checkbox ui-checkbox-info">
+                    <input type="checkbox">
+                    <span class="input-span"></span>Remember me</label>
+                <a href="forgot_password.html">Forgot password?</a>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-info btn-block" type="submit">Login</button>
+            </div>
+        
+        </form>
+    </div>
+    <!-- BEGIN PAGA BACKDROPS-->
+    <div class="sidenav-backdrop backdrop"></div>
+    <div class="preloader-backdrop">
+        <div class="page-preloader">Loading</div>
+    </div>
+    <!-- END PAGA BACKDROPS-->
+    <!-- CORE PLUGINS -->
+    <script src="{{asset('asset')}}/assets/vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="{{asset('asset')}}/assets/vendors/popper.js/dist/umd/popper.min.js" type="text/javascript"></script>
+    <script src="{{asset('asset')}}/assets/vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL PLUGINS -->
+    <script src="{{asset('asset')}}/assets/vendors/jquery-validation/dist/jquery.validate.min.js" type="text/javascript"></script>
+    <!-- CORE SCRIPTS-->
+    <script src="{{asset('asset')}}/assets/js/app.js" type="text/javascript"></script>
+    <!-- PAGE LEVEL SCRIPTS-->
+    <script type="text/javascript">
+        $(function() {
+            $('#login-form').validate({
+                errorClass: "help-block",
+                rules: {
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    password: {
+                        required: true
+                    }
+                },
+                highlight: function(e) {
+                    $(e).closest(".form-group").addClass("has-error")
+                },
+                unhighlight: function(e) {
+                    $(e).closest(".form-group").removeClass("has-error")
+                },
+            });
+        });
+    </script>
+</body>
+
+</html>
