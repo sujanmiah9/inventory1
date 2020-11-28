d250961cfcaeb4b5a5abb4b8738f874a33193322
jobairahmed <ryleejane663@gmail.com>
2020-11-28 16:29:35 +0600

    jkdkdj

--- resources/views/partial/footer.blade.php
+++ resources/views/partial/footer.blade.php
@@ -1,5 +1,7 @@
-<footer class="page-footer">
-    <div class="font-13">2018 © <b>AdminCAST</b> - All rights reserved.</div>
-    <a class="px-4" href="http://themeforest.net/item/adminca-responsive-bootstrap-4-3-angular-4-admin-dashboard-template/20912589" target="_blank">BUY PREMIUM</a>
+
+
+<footer class=" p-3 bg-dark text-white text-center ">
+    <div class="">{{date('Y')}} © <b>Inventory & POS</b> - All rights reserved.</div>
+    <a class="px-4 text-white" href="https://github.com/sujanmiah9/inventory.git" target="_blank">Git Hub Link</a>
     <div class="to-top"><i class="fa fa-angle-double-up"></i></div>
 </footer>
