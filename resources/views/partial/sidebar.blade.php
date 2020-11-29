9ab855cbc5a3b5892fc2d97bf8baa41a02b9b971
jobairahmed <ryleejane663@gmail.com>
2020-11-29 10:45:56 +0600

    edit

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -5,7 +5,7 @@
             <img src="{{asset('asset')}}/assets/img/admin-avatar.png" width="45px" />
             </div>
             <div class="admin-info">
-                <div class="font-strong">Wahid Sujan</div><small>Administrator</small></div>
+                <div class="font-strong">Admin</div><small>Administrator</small></div>
         </div>
         <ul class="side-menu metismenu">
             <li>
